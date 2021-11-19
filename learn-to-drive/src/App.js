067b46dee7ef2a70ba9210e4d0e475417bbd499e
Mohammed Yasin Mulla <yasinspy1999@gmail.com>
2021-11-19 21:31:03 +0530

    Completed learn-to-drive using .map() funtion

--- learn-to-drive/src/App.js
+++ learn-to-drive/src/App.js
@@ -1,38 +1,71 @@
-import React from 'react'
-import logo from './images/logo.png'
-import main from './images/main.jpg'
-
-
+import React from "react";
+import logo from "./images/logo.png";
+import main from "./images/main1.jpg";
+import drift from "./images/racing.jpg";
+import offroading from './images/offRoading.jpg'
+import raceing from './images/racing.jpg'
+import data from './data.js'
 
 function Navbar() {
-    return(
-        <nav className='navbar--'>
-            <img className='navbar--img' src={logo} alt="logo" width='40px'/>
-            <h3>Tame your beast</h3>
-        </nav>
-    )
+  return (
+    <nav className="navbar">
+      <img className="navbar--img" src={logo} alt="logo" width="40px" />
+      <h3>SUPER SPEED</h3>
+    </nav>
+  );
 }
 
 function Main() {
-    return(
-        <div className="main--">
-            <img src={main} alt="main images" className="main--img" />
-        </div>
-    )
+  return (
+    <div className="main">
+      <img src={main} alt="main images" className="main--img" />
+      <h3>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias
+        voluptatem eaque ipsum labore magni, nihil laudantium minus architecto
+        tempore odit, excepturi veritatis sequi.
+      </h3>
+    </div>
+  );
+}
+
+function Card(props) {
+  return (
+      <div className="card">
+       
+          <img src={raceing}className="card--img" />
+          <div className="card--stats">
+             
+              <span className='card--rating'>Rating {props.rating}</span>
+              <span className="gray card--review">({props.review}) • </span>
+              <span className="gray">{props.location}</span>
+          </div>
+          <div className="card--info">
+            <h4 className="card--title">{props.title}</h4>
+            <p className='card--description'>{props.description} </p>
+            <p className="card--price"><span className="bold"><i>From </i>${props.price}</span> / person</p>
+          </div>
+      </div>)
 }
 
 
+// console.log(data)
+const cardsList = data.map(item => {
+  return <Card key={item.id} title={item.title} img={item.img} price={item.price} rating={item.rating} review={item.review
+  } location={item.location}  description={item.description}/>
+})
 
-
+console.log(cardsList)
 function App() {
-    return(
-        <div>
-            <Navbar/>
-            <Main/>
-            <h1>Hello There!!!</h1>
-        </div>
-        
-        
-    )
+  return (
+    <div>
+      <Navbar />
+      <Main />
+      
+      <div className="cards">
+          {cardsList}
+      </div>
+     
+    </div>
+  );
 }
 export default App;
---
+++ learn-to-drive/src/data.js
@@ -0,0 +1,35 @@
+export default [
+    {
+        id : 1,
+        title : 'Drifting',
+        img: `{racing}`,
+        price: 154 ,
+        rating : 3.5,
+        review: 8,
+        location : 'Dubai',
+        description : 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vero assumenda doloremque nesciunt neque alias reiciendis?'
+    },
+    {
+        id : 2,
+        title : 'Racing',
+        img: 1,
+        price: 194 ,
+        rating : 5,
+        review: 100,
+        location : 'Abu-Dhabi',
+        description: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quod architecto similique voluptas praesentium? Tempore, voluptas.'
+
+    },
+    {
+        id : 3,
+        title : 'Off Roading',
+        img: 1,
+        price: 200 ,
+        rating : 4.5,
+        review: 350,
+        location : 'Abu-Dhabi',
+        description: 'Lorem, ipsum dolor  elit. Quod architecto similique voluptas praesentium? Tempore, voluptas.'
+
+    }
+
+]
