cc49e6122fd4c0346ef98bef2b0037d98b365d0a
Mohammed Yasin Mulla <yasinspy1999@gmail.com>
2021-11-21 22:29:03 +0530

    Added the funtion to change the images using React.useState methord

--- meme-generator/src/Main.js
+++ meme-generator/src/Main.js
@@ -1,13 +1,28 @@
 import React from 'react'
+import memesdata from './memesData'
+
+console.log(memesdata)
+
+
+
 
 export default function Meme(){
+    const [url, setValue] = React.useState("https://i.imgflip.com/30b1gx.jpg")
+    function getMemeImage(){
+        const arraymeme = memesdata.data.memes
+        const randomNumber = Math.floor(Math.random() * arraymeme.length)
+        setValue(() =>arraymeme[randomNumber].url )
+    
+    }
+    
     return(
         <div className="main">
             <div className="main--input">
                 <input type="text" placeholder='top text' />
                 <input type="text" placeholder='bottom text' />
             </div>
-            <button className='main--button'>Get The Meme  image here</button>
+            <button onClick={getMemeImage} className='main--button'>Get The Meme  image here</button>
+            <img className='main--img' src={url} alt="" />
         </div>
     )
 }
