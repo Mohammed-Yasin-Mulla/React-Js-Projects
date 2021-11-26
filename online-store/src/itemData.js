e4d87f53885bb64da58384076d67447e6f4179c9
Mohammed Yasin Mulla <yasinspy1999@gmail.com>
2021-11-26 14:38:15 +0530

    Added product to shop Cart funtion

--- online-store/src/itemData.js
+++ online-store/src/itemData.js
@@ -2,22 +2,22 @@ export default {
     "data" : [
         {
             "img": "product",
-            "name" : "product",
+            "name" : "product1",
             "price": "2405",
         },
         {
             "img": "product2",
-            "name" : "product",
+            "name" : "product2",
             "price": "2465",
         },
         {
             "img": "product3",
-            "name" : "product",
+            "name" : "product3",
             "price": "2445",
         },
         {
             "img": "product2",
-            "name" : "product",
+            "name" : "product4",
             "price": "245",
         },
     ]
